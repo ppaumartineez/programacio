9a8c6d7fe06b322bc16320e467d336f802563fe9
ppaumartineez <115455239+ppaumartineez@users.noreply.github.com>
2022-11-07 11:22:15 +0100

    Update random2.py

--- random2.py
+++ random2.py
@@ -1,29 +1,28 @@
 import random
 
-#cream el random
 x = random.randint(1, 100)
-print("el nombre es", x)
 
+endevinat = True
 
-endevinat = False
-#comptador de torns
 torn = 1
 llista = []
 
 
-while (endevinat):
+
+
+while (endevinat) :        
     torn += 1
-    y = int(input("demana numero")) 
-    if (x>y):
-        print ("es petit però valent")
-    elif (x<y):
-        print ("es gros")
-    else:
-        endevinat = True
-    llista.append(y)
+    y = int(input("Adivina el numero escrivint-hi un: "))
    
+    if (x>y):
+        print("Es petit, torna a provar")
+    elif (x<y):
+        print("Es gran, torna a provar")
+    else:
+        endevinat = False
+    
+    llista.append(y)
 
-
-print("molt be has endivinat")
-print("has emprat", torn, "torns")
+print("Enorabona! Aconseguit en" , torn , "vegades.")
 print(llista)
+print("En efectiu x era", x)
